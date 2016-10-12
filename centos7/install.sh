a2c57158ac388e64980c4a15081ab68217487c6d
Joe Lei <thezero12@hotmail.com>
2016-10-12 23:29:09 +0800

    init commit

---
+++ centos7/install.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+su - root
+rm -rf /etc/yum.repos.d/*
+cp /vagrant/*repo /etc/yum.repos.d
+yum clean all
+yum makecache
+yum install -y gcc make kernel-devel vim psmisc net-tools wget
+mount -t iso9660 -o loop,user /vagrant/VBoxGuestAdditions.iso  /mnt
+cd /mnt
+bash VBoxLinuxAdditions.run install
+reboot
