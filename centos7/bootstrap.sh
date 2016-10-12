c682da64a847683a8789577bd916e73ec19713ab
Joe Lei <thezero12@hotmail.com>
2016-10-13 00:08:37 +0800

    add bootstrap.sh

--- centos7/bootstrap.sh
+++ centos7/bootstrap.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 rm -rf /etc/yum.repos.d/*
-cp /vagrant/*repo /etc/yum.repos.d
+curl https://raw.githubusercontent.com/ifooth/vagrant/master/centos7/Centos-7.repo > /etc/yum.repos.d/Centos-7.repo
+curl https://raw.githubusercontent.com/ifooth/vagrant/master/centos7/epel.repo > /etc/yum.repos.d/epel.repo
 yum clean all
 yum makecache
 yum install -y gcc make kernel-devel vim psmisc net-tools wget
