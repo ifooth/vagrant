b0477acf4bb7b1930f2a0b0cbd2dbfed630ad343
Joe Lei <thezero12@hotmail.com>
2016-10-15 09:00:05 +0800

    add locale

--- centos7/bootstrap.sh
+++ centos7/bootstrap.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 echo 'start install yum repo'
 rm -rf /etc/yum.repos.d/*
-curl https://raw.githubusercontent.com/ifooth/vagrant/master/centos7/Centos-7.repo > /etc/yum.repos.d/Centos-7.repo
-curl https://raw.githubusercontent.com/ifooth/vagrant/master/centos7/epel.repo > /etc/yum.repos.d/epel.repo
+curl https://raw.githubusercontent.com/ifooth/vagrant/master/centos7/Centos-7.repo -o /etc/yum.repos.d/Centos-7.repo -s
+curl https://raw.githubusercontent.com/ifooth/vagrant/master/centos7/epel.repo -o /etc/yum.repos.d/epel.repo -s
 yum clean all
 yum makecache
 
@@ -13,5 +13,8 @@ echo 'start install VBoxGuestAdditions'
 mount -t iso9660 -o loop,user /vagrant/VBoxGuestAdditions.iso  /mnt
 cd /mnt
 bash VBoxLinuxAdditions.run install
+
+localectl set-locale LANG=en_US.UTF-8
+timedatectl set-timezone Asia/Shanghai
 
 echo 'bootstrap finish'
